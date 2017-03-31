206f20a41bb3354475186a0a5399dcf24ef06ec2
William Wegerson <github_ww@wegerson.com>
2017-03-31 08:54:26 -0600

    Found primary option description failure; no fix yet

--- TestCommandLine/TestDescription.cs
+++ TestCommandLine/TestDescription.cs
@@ -24,5 +24,34 @@ namespace TestCommandLine
                               .AssociateWithSubOptions("D")
                               .Execute();
         }
+
+
+        [TestMethod]
+        public void TestMultipleOptions()
+        {
+            string info = "Operations to be performed: ";
+
+            CommandLineManager.Instantiation()
+                  .DisplayBeforeDescription((o) => Console.WriteLine($"{info}{Environment.NewLine}"))
+                  .DisplayProductAndVersion()
+                  .DisplayDescriptionOnNoOperation()
+                  .AddOption("U", "Upload a file to Azure", clm => Console.WriteLine("Uploading"))
+                  .AddValidation(clm => true)
+                  .AssociateWithSubOptions("F", "C", "B")
+                  .AddOptionRequiresData("F", "File name of the file.")
+                  .AddOptionRequiresData("C", "(Azure) Container to use or create in Azure.")
+                  .AddOptionRequiresData("B", "(Azure) Blob name to use. If none specified, the filename will be used.")
+                  .AddOption("D", "Download a file From Azure", clm => Console.WriteLine("Downloading"))
+                  .AddValidation(clm => true)
+                  .AssociateWithSubOptions("F", "C", "B")
+                  .AddOption("S", "Show credentials to be used.",clm => Console.WriteLine("Showing"))
+                  .Execute();
+
+
+
+
+
+        }
+
     }
 }
