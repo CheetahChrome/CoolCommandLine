926456828ee0ff2bab10bae9aa60ddb45a6be01a
William Wegerson <github_ww@wegerson.com>
2017-04-07 18:51:03 -0600

    Commenting

--- TestCommandLine/TestDescription.cs
+++ TestCommandLine/TestDescription.cs
@@ -32,21 +32,21 @@ namespace TestCommandLine
             string info = "Operations to be performed: ";
 
             CommandLineManager.Instantiation()
-                  .DisplayBeforeDescription((o) => Console.WriteLine($"{info}{Environment.NewLine}"))
-                  .DisplayProductAndVersion()
-                  .DisplayDescriptionOnNoOperation()
-                  .AddOption("Q", "Queue the App into named pipe remote command mode", clm => Console.WriteLine("Queue Mode"))
-                  .AddOption("U", "Upload a file to Azure", clm => Console.WriteLine("Uploading"))
-                  .AddValidation(clm => true)
-                  .AssociateWithSubOptions("F", "C", "B")
-                  .AddOptionRequiresData("F", "File name of the file.")
-                  .AddOptionRequiresData("C", "(Azure) Container to use or create in Azure.")
-                  .AddOptionRequiresData("B", "(Azure) Blob name to use. If none specified, the filename will be used.")
-                  .AddOption("D", "Download a file From Azure", clm => Console.WriteLine("Downloading"))
-                  .AddValidation(clm => true)
-                  .AssociateWithSubOptions("F", "C", "B")
-                  .AddOption("S", "Show credentials to be used.",clm => Console.WriteLine("Showing"))
-                  .Execute();
+                              .DisplayBeforeDescription((o) => Console.WriteLine($"{info}{Environment.NewLine}"))
+                              .DisplayProductAndVersion()
+                              .DisplayDescriptionOnNoOperation()
+                              .AddOption("Q", "Queue the App into named pipe remote command mode", clm => Console.WriteLine("Queue Mode"))
+                              .AddOption("U", "Upload a file to Azure", clm => Console.WriteLine("Uploading"))
+                              .AddValidation(clm => true)
+                              .AssociateWithSubOptions("F", "C", "B")
+                              .AddOptionRequiresData("F", "File name of the file.")
+                              .AddOptionRequiresData("C", "(Azure) Container to use or create in Azure.")
+                              .AddOptionRequiresData("B", "(Azure) Blob name to use. If none specified, the filename will be used.")
+                              .AddOption("D", "Download a file From Azure", clm => Console.WriteLine("Downloading"))
+                              .AddValidation(clm => true)
+                              .AssociateWithSubOptions("F", "C", "B")
+                              .AddOption("S", "Show credentials to be used.",clm => Console.WriteLine("Showing"))
+                              .Execute();
 
 
 
